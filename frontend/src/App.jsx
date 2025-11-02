4ce6ba993a212a06448401704f241c7c7afd2f33
Cassio Passos <passoscassio29@gmail.com>
2025-11-02 16:45:33 -0300

    ajustes de projeto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@/lib/supabase";
 
 import Auth from "./pages/Auth";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 
 const App = () => (
+  <AuthProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<Auth />} />
@@ -12,6 +14,7 @@ const App = () => (
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
+  </AuthProvider>
 );
 
 export default App;
